b106b4dc4f31a9a9714cde592c5816ee3e65de80
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:46:07 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -7,6 +7,7 @@ clean <- function(states)
     # clean the rows and columns
     states <- states[-c(1,last_row), -c(1:4)]
     colnames(states) <- c("statename","population","popOver18","percentOver18")
+    
     return states
     
 }
