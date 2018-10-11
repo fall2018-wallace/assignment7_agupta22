3ef30cb5e660a14562fe0ff76831992eb4963f31
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:40:49 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -4,8 +4,9 @@ uncleaned_data <- raw_data
 clean <- function(states)
 {
     last_row <- nrow(states)
-    # clean the first and last row 
-    
+    # clean the rows and columns
+    states <- states[-c(1,last_row), -c(1:4)]
+    colnames(states) <- c("statename","population","popOver18","percentOver18")
 }
 
 Final_data <- clean(uncleaned_data)
