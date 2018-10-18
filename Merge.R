e468b9e993802ecf78a1b01dc1ecb193101eaebe
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 07:13:00 -0400

    Saving Merge.R

--- Merge.R
+++ Merge.R
@@ -49,6 +49,12 @@ murder_map <- murder_map + expand_limits(x=US$long, y=US$lat)
 
 #population_map <- murder_map + geom_point (aes(x=dfmerge$state_lat, y=dfmerge$state_lon, size=dfmerge$POPESTIMATE2017), color="red")      
 #population_map <- murder_map + coord_map()
+population_map<-ggplot(dfmerge , aes(map_id=stateName))
+population_map<- population_map + geom_map(map = US,aes(fill=dfmerge$population))
+population_map<- population_map + expand_limits(x= dfmerge$x,y=dfmerge$y) 
+population_map<- population_map + geom_point(data=dfmerge, aes(x=dfmerge$x,y=dfmerge$y,size=dfmerge$population), color ="Red")
+population_map<- population_map + coord_map() + ggtitle("Map of USA (Population)")
+population_map
 
 #Step D: Zoom the map
 #6)	Repeat step C, but only show the states in the north east
