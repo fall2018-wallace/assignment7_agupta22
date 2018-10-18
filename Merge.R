48bcf6d59a6b8f567e4f2b791cc33b758f896125
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 06:29:35 -0400

    Saving Merge.R

--- Merge.R
+++ Merge.R
@@ -8,3 +8,12 @@ dfmerge <- cbind(NState, arrests)
 dfmerge
 #2)	Add the area of each state, and the center of each state, to the merged dataframe, using the 'state.center', 'state.center' and 'state.name' vectors
 center_state <- state.center
+state_latitude <- center_state[["x"]]                     
+state_longitude <- center_state[["y"]]                
+statearea <- state.area                             
+
+dfmerge$state_lat <- state_latitude                  
+dfmerge$state_lon <- state_longitude
+dfmerge$state_area <- statearea
+
+dfmerge$NAME <- tolower(dfmerge$NAME)               
