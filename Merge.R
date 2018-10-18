aca6352e2bc316e00de11f10ecb40c4b3fb1d9af
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 07:01:06 -0400

    Saving Merge.R

--- Merge.R
+++ Merge.R
@@ -55,7 +55,7 @@ population_map <- murder_map + coord_map()
 #Hint: get the lat and lon of new york city
 #Hint: set the xlim and ylim to NYC +/- 10
 
-nyc_lat_long <- geocode("new york city, ny", source = "dsk")      #getting lat lon of nyc
+nyc_lat_long <- geocode("new york city, ny", source = "google")      
 north_east_map <- ggplot(dfmerge, aes(map_id=statename)) + geom_map(map=US , aes(fill=Murder)) + expand_limits(x=dfmerge$state_lat , y=dfmerge$state_lon) + coord_map() + ggtitle ("Northeast of US") +geom_point (aes(x=dfmerge$state_lat, y=dfmerge$state_lon, size=dfmerge$POPESTIMATE2017), color="red")     #ploting murder and population in map as before
 north_east_map <- north_east_map + xlim(nyc_lat_long$lon-10, nyc_lat_long$lon+10) + ylim (nyc_lat_long$lat-10, nyc_lat_long$lat+10)      
 
