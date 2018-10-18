05d0766bbb6310b13d1d3b743871bdf3db800ea5
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 07:07:07 -0400

    Saving Merge.R

--- Merge.R
+++ Merge.R
@@ -31,7 +31,7 @@ area_map <- area_map + geom_map (map=US, aes(fill=dfmerge$state_area))
 area_map
 
 area_map <- area_map + expand_limits(x=US$long, y=US$lat)       
-area_map
+
 
 #Step C: Create a color shaded map of the U.S. based on the Murder rate for each state 
 #4)	Repeat step B, but color code the map based on the murder rate of each state.
@@ -43,7 +43,7 @@ murder_map <- murder_map + geom_map (map=US, aes(fill=dfmerge$Murder))
 
 
 murder_map <- murder_map + expand_limits(x=US$long, y=US$lat)
-murder_map
+
 
 #5)	 Show the population as a circle per state (the larger the population, the larger the circle), using the location defined by the center of each state
 
@@ -56,7 +56,7 @@ population_map <- murder_map + coord_map()
 #Hint: set the xlim and ylim to NYC +/- 10
 library(ggmap)
 nyc_lat_long <- geocode("new york city, ny", source = "dsk")      
-north_east_map <- ggplot(dfmerge, aes(map_id=statename)) + geom_map(map=US , aes(fill=Murder)) + expand_limits(x=dfmerge$state_lat , y=dfmerge$state_lon) + coord_map() + ggtitle ("Northeast of US") +geom_point (aes(x=dfmerge$state_lat, y=dfmerge$state_lon, size=dfmerge$POPESTIMATE2017), color="red")     #ploting murder and population in map as before
+north_east_map <- ggplot(dfmerge, aes(map_id=statename)) + geom_map(map=US , aes(fill=Murder)) + expand_limits(x=dfmerge$state_lat , y=dfmerge$state_lon) + coord_map() + ggtitle ("Northeast of US") +geom_point (aes(x=dfmerge$state_lat, y=dfmerge$state_lon, size=dfmerge$POPESTIMATE2017), color="red")     
 north_east_map <- north_east_map + xlim(nyc_lat_long$lon-10, nyc_lat_long$lon+10) + ylim (nyc_lat_long$lat-10, nyc_lat_long$lat+10)      
 
 
